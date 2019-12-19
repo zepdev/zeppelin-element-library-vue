e7ad5a82a219bbfb61edaa35df28ddad60b97dd8
Alyssa <ca.butler10@gmail.com>
2019-12-19 15:49:36 +0100

    initial commit

---
+++ dist/zel-vue.ssr.js
@@ -0,0 +1,187 @@
+'use strict';Object.defineProperty(exports,'__esModule',{value:true});//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+var script = {
+  name: "ZelVueButton",
+
+  props: {
+    variant: {
+      type: String,
+      default: "primary"
+    },
+    icon: {
+      type: String,
+      default: ""
+    },
+    size: String,
+    disabled: Boolean,
+    fullWidth: Boolean
+  },
+  computed: {
+    buttonDisabled: function buttonDisabled() {
+      return this.disabled || (this.elForm || {}).disabled;
+    }
+  },
+  methods: {
+    handleClick: function handleClick(evt) {
+      this.$emit("click", evt);
+    }
+  }
+};function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+    if (typeof shadowMode !== 'boolean') {
+        createInjectorSSR = createInjector;
+        createInjector = shadowMode;
+        shadowMode = false;
+    }
+    // Vue.extend constructor export interop.
+    var options = typeof script === 'function' ? script.options : script;
+    // render functions
+    if (template && template.render) {
+        options.render = template.render;
+        options.staticRenderFns = template.staticRenderFns;
+        options._compiled = true;
+        // functional template
+        if (isFunctionalTemplate) {
+            options.functional = true;
+        }
+    }
+    // scopedId
+    if (scopeId) {
+        options._scopeId = scopeId;
+    }
+    var hook;
+    if (moduleIdentifier) {
+        // server build
+        hook = function (context) {
+            // 2.3 injection
+            context =
+                context || // cached call
+                    (this.$vnode && this.$vnode.ssrContext) || // stateful
+                    (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+            // 2.2 with runInNewContext: true
+            if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+                context = __VUE_SSR_CONTEXT__;
+            }
+            // inject component styles
+            if (style) {
+                style.call(this, createInjectorSSR(context));
+            }
+            // register component module identifier for async chunk inference
+            if (context && context._registeredComponents) {
+                context._registeredComponents.add(moduleIdentifier);
+            }
+        };
+        // used by ssr in case component is cached and beforeCreate
+        // never gets called
+        options._ssrRegister = hook;
+    }
+    else if (style) {
+        hook = shadowMode
+            ? function (context) {
+                style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
+            }
+            : function (context) {
+                style.call(this, createInjector(context));
+            };
+    }
+    if (hook) {
+        if (options.functional) {
+            // register for functional component in vue file
+            var originalRender = options.render;
+            options.render = function renderWithStyleInjection(h, context) {
+                hook.call(context);
+                return originalRender(h, context);
+            };
+        }
+        else {
+            // inject component registration as beforeCreate hook
+            var existing = options.beforeCreate;
+            options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+        }
+    }
+    return script;
+}/* script */
+var __vue_script__ = script;
+
+/* template */
+var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('button',{staticClass:"zep-button",class:[
+    _vm.variant ? 'zep-button-' + _vm.variant : '',
+    _vm.size === 'small' ? 'zep-button--small' : '',
+    _vm.fullWidth ? 'zep-button--small' : ''
+  ],attrs:{"disabled":_vm.buttonDisabled,"variant":_vm.variant},on:{"click":_vm.handleClick}},[_vm._ssrNode("<span"+(_vm._ssrClass("zep-button__text",[_vm.variant === 'tertiary' ? 'zep-button__text--tertiary' : '']))+">","</span>",[(_vm.$slots.default)?_vm._ssrNode("<span>","</span>",[_vm._t("default")],2):_vm._e()])])};
+var __vue_staticRenderFns__ = [];
+
+  /* style */
+  var __vue_inject_styles__ = undefined;
+  /* scoped */
+  var __vue_scope_id__ = undefined;
+  /* module identifier */
+  var __vue_module_identifier__ = "data-v-20c382c6";
+  /* functional template */
+  var __vue_is_functional_template__ = false;
+  /* style inject */
+  
+  /* style inject SSR */
+  
+  /* style inject shadow dom */
+  
+
+  
+  var __vue_component__ = normalizeComponent(
+    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+    __vue_inject_styles__,
+    __vue_script__,
+    __vue_scope_id__,
+    __vue_is_functional_template__,
+    __vue_module_identifier__,
+    false,
+    undefined,
+    undefined,
+    undefined
+  );/* eslint-disable import/prefer-default-export */var components=/*#__PURE__*/Object.freeze({__proto__:null,ZelVueButton: __vue_component__});// Import vue components
+
+// install function executed by Vue.use()
+function install(Vue) {
+  if (install.installed) { return; }
+  install.installed = true;
+  Object.keys(components).forEach(function (componentName) {
+    Vue.component(componentName, components[componentName]);
+  });
+}
+
+// Create module definition for Vue.use()
+var plugin = {
+  install: install,
+};
+
+// To auto-install when vue is found
+/* global window global */
+var GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}exports.ZelVueButton=__vue_component__;exports.default=plugin;
---
+++ src/lib-components/zel-vue-button.vue
@@ -0,0 +1,51 @@
+<template>
+  <button
+    class="zep-button"
+    @click="handleClick"
+    :disabled="buttonDisabled"
+    :variant="variant"
+    :class="[
+      variant ? 'zep-button-' + variant : '',
+      size === 'small' ? 'zep-button--small' : '',
+      fullWidth ? 'zep-button--small' : ''
+    ]"
+  >
+    <span
+      class="zep-button__text"
+      :class="[variant === 'tertiary' ? 'zep-button__text--tertiary' : '']"
+    >
+      <span v-if="$slots.default">
+        <slot></slot>
+      </span>
+    </span>
+  </button>
+</template>
+<script>
+export default {
+  name: "ZelVueButton",
+
+  props: {
+    variant: {
+      type: String,
+      default: "primary"
+    },
+    icon: {
+      type: String,
+      default: ""
+    },
+    size: String,
+    disabled: Boolean,
+    fullWidth: Boolean
+  },
+  computed: {
+    buttonDisabled() {
+      return this.disabled || (this.elForm || {}).disabled;
+    }
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit("click", evt);
+    }
+  }
+};
+</script>
